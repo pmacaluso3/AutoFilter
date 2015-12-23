43dd650a9a95656d6528a1d9077b32b67aa1db2a
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-12-23 14:35:17 -0600

    trying to view home page after deploy, taking out parse and populate files

--- app/populate/car_populator_for_user.rb
+++
@@ -1,5 +0,0 @@
-class CarPopulatorForUser < ActiveRecord::Base
-  def self.populate_cars_for_user(user)
-    user.zip
-  end
-end
--- app/populate/cl_car_url_builder.rb
+++
@@ -1,7 +0,0 @@
-class ClCarUrlBuilder
-  def build_from_array(array)
-    array.each do |url_data|
-      ClCarUrl.find_or_create_by(text: url_data['text'], href: url_data['href'])
-    end
-  end
-end
